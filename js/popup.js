d8d0bf76e33b41f3a91a84bd639031c4c8d367d4
PabloArosa <118744214+PabloArosa@users.noreply.github.com>
2022-12-21 17:53:20 +0100

    Proyecto terminado ya por fin menos mal diosito

---
+++ js/popup.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function() {
+    var result = confirm("Al final de la página hay un reproductor de música con lofi para hacer la lectura más amena!");
+    
+  });
---
+++ js/volumen.js
@@ -0,0 +1,2 @@
+var audio = document.getElementById("beat");
+audio.volume = 0.2; // Establecer volumen inicial al 20%
